e6fe164a6d11391e26dfb90b628cbc2a8c1c2e71
narumichikubo <narumichi7373@gmail.com>
2023-04-27 17:54:28 +0900

    init

---
+++ SwiftUIPlayGround/SwiftUIPlayGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlayGroundApp.swift
+//  SwiftUIPlayGround
+//
+//  Created by Narumichi Kubo on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlayGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
